37eb7b027183ac0ae74c27c6098058ffc59a365f
Michał Nowak <michal@enuai.pl>
2022-07-22 08:59:30 +0200

    Initial Commit

---
+++ Source Switcher/Source_SwitcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  Source_SwitcherApp.swift
+//  Source Switcher
+//
+//  Created by Michał Nowak on 7/22/22.
+//
+
+import SwiftUI
+
+@main
+struct Source_SwitcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
